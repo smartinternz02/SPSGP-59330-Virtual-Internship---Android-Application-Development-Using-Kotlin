e279b72812af34ed2880668134c38f0dfb3866dc
Veera Lakshmi Vaidadi <105004498+Lakshmiveeru@users.noreply.github.com>
2022-10-13 22:00:51 +0530

    Delete NearbyPlacesResponse.kt

--- NearByPlaces/Kotlin files/api/NearbyPlacesResponse.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Google LL
-package com.rahulpa.myapplication.api
-
-import com.rahulpa.myapplication.model.Place
-import com.google.gson.annotations.SerializedName
-
-data class NearbyPlacesResponse(
-   @SerializedName("results") val results: List<Place>
-)
